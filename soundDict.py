4a45723e6eae07a39d9d66232cb2451c7deca6dc
mika <mika@mr-framework.localdomain>
2023-11-25 09:51:28 +0100

    add soundDict

---
+++ soundDict.py
@@ -0,0 +1,12 @@
+# Mapping of keys to sound file names
+sounds = {
+    'a': 'ahhhh.mp3',
+    'b': 'beback.mp3',
+    'c': 'boom.mp3',
+    'd': 'bruh.mp3',
+    'e': 'calming.mp3',
+    'f': 'clap.mp3',
+    'g': 'gae.mp3',
+    'h': 'giveuup.mp3',
+    'i': 'lenz.mp3'
+}
