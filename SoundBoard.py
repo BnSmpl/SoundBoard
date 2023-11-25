d5e4366322e2e8b3dbb6f9c5572a7ec1a61bf2d8
mika <mika@mr-framework.localdomain>
2023-11-25 09:52:21 +0100

    refactor to fix keyboard listener

--- SoundBoard.py
+++ SoundBoard.py
@@ -1,13 +1,28 @@
-import keyboard
-import threading
-import time
+import sys
+import tty
+import termios
+from soundDict import sounds
 
+def getch():
+    # Fetch a character from the stdin without requiring a newline
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
 
 try:
-    print("Program is running. Press Ctrl+C to exit.")
-    while True:# main loop
-        time.sleep(1) #remove when adding real code
-        
+    print("Program is running. Press 'q' to exit.")
+    while True:
+        char = getch()
+        if char in sounds:
+            print(f"Playing sound: {sounds[char]}")
+        if char == 'q':  # Exit condition
+            break
 except KeyboardInterrupt:
-    print("Ctrl+C pressed. Exiting the program.")
+    print("Exiting the program.")
 
+print("Program exited.")
