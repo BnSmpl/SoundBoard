aa542431047f20289262bc1cca45e29b30e9fd2e
mika <mika@mr-framework.localdomain>
2023-11-25 10:49:23 +0100

    refactor for keyboard listener

--- SoundBoard.py
+++ SoundBoard.py
@@ -2,26 +2,19 @@ import sys
 import tty
 import termios
 from soundDict import sounds
-
-def getch():
-    # Fetch a character from the stdin without requiring a newline
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    try:
-        tty.setraw(sys.stdin.fileno())
-        ch = sys.stdin.read(1)
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-    return ch
+from listeners.keyboard_listener import listen_for_key
+from utils.signals import Signal, SignalType
 
 try:
     print("Program is running. Press 'q' to exit.")
     while True:
-        char = getch()
-        if char in sounds:
-            print(f"Playing sound: {sounds[char]}")
-        if char == 'q':  # Exit condition
-            break
+        signal = listen_for_key()
+        if signal:
+        
+            if signal.signal_type == SignalType.QUIT:  # Exit condition
+                break
+            # Process other signals as needed
+            print(f"Signal received: {signal.signal_type}")
 except KeyboardInterrupt:
     print("Exiting the program.")
 
