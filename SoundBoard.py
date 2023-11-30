d1d56c2eaa65ac948658f7532a0a1be37fb9d2d0
BnSmpl <68922064+BnSmpl@users.noreply.github.com>
2023-11-30 12:11:12 +0100

    Feat/basic sound board (#2)
    add gpio and keyboard listeners and monitors

---
+++ SoundBoard.py
@@ -0,0 +1,51 @@
+import queue
+import threading
+from soundDict import sounds
+from listeners.keyboard_listener import listen_for_key
+from listeners.gpio_listener import start_gpio_listener, SignalType
+from utils.signals import Signal, SignalType
+
+# Function to process signals
+def process_signal(signal):
+    if signal and signal.signal_type != SignalType.QUIT:
+        sound_info = signal.signal_type.name + " | " + sounds.get(signal.signal_type.name)
+        print(sound_info)  # Replace this with actual sound playing logic
+    elif signal and signal.signal_type == SignalType.QUIT:
+        return True
+    return False
+
+def main():
+    print("Program is running. Press 'q' to exit.")
+
+    # Shared queue for signals
+    signal_queue = queue.Queue()
+    
+    # Start GPIO listener in a separate thread
+    gpio_thread = threading.Thread(target=start_gpio_listener, args=(signal_queue,))
+    gpio_thread.start()
+
+    try:
+        while True:
+            # Check for and process any signals in the queue
+            try:
+                signal = signal_queue.get_nowait()
+                if process_signal(signal):
+                    break
+            except queue.Empty:
+                pass
+
+            # Also check for keyboard signals
+            keyboard_signal = listen_for_key()
+            if keyboard_signal:
+                signal_queue.put(keyboard_signal)
+
+    except KeyboardInterrupt:
+        print("Exiting the program.")
+
+    finally:
+        gpio_thread.join()  # Ensure GPIO thread is properly closed
+
+    print("Program exited.")
+
+if __name__ == "__main__":
+    main()
---
+++ listeners/keyboard_listener.py
@@ -0,0 +1,35 @@
+import sys
+import tty
+import termios
+from utils.signals import Signal, SignalType
+
+key_to_signal = {
+    '1': SignalType.SOUND1,
+    '2': SignalType.SOUND2,
+    '3': SignalType.SOUND3,
+    '4': SignalType.SOUND4,
+    '5': SignalType.SOUND5,
+    '6': SignalType.SOUND6,
+    '7': SignalType.SOUND7,
+    '8': SignalType.SOUND8,
+    '9': SignalType.SOUND9,
+    
+    'r': SignalType.REC_BTN,
+    's': SignalType.STOP_SOUND,
+    'q': SignalType.QUIT
+}
+
+def listen_for_key():
+    """
+    Listens for a single key press and returns the corresponding Signal.
+    Returns None if the key is not mapped to a signal.
+    """
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+
+    return Signal(key_to_signal[ch]) if ch in key_to_signal else None
