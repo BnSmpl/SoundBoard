2bdc1218889350195b0d368e1ab222754982d570
Tronik <thetronikhd@gmail.com>
2023-12-14 14:47:22 +0100

    change name to mp3

--- utils/SampleAudioPlayer.py
+++ utils/SampleAudioPlayer.py
@@ -2,10 +2,10 @@ from mpyg321.mpyg321 import MPyg321Player
 import time
 
 
-def play_mp3(signal):
-    print(signal)
+def play_mp3(mp3):
+    print(mp3)
     soundPath = './Sounds/'
     player = MPyg321Player()
-    player.play_song(soundPath + signal)
+    player.play_song(soundPath + mp3)
     while True:
         time.sleep(1)
