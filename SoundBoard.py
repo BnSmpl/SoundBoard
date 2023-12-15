bd0f7a04886deba1795a578b03157347a30b879e
sklepp <shirin.klepp@milchundzucker.de>
2023-12-15 12:33:26 +0100

    try to make it work :)

--- SoundBoard.py
+++ SoundBoard.py
@@ -3,11 +3,11 @@ from utils.SampleAudioPlayer import play_mp3
 
 
 def main():
-    play_mp3(listener())
+    try:
+        while True:
+            play_mp3(listener())
+    except KeyboardInterrupt:
+        print("\nBye bye!")
 
 
-try:
-    while True:
-        main()
-except KeyboardInterrupt:
-    print("\nBye bye!")
+main()
