65333d82fa53ebede28c572b6813e0a2e9864b53
Tronik <thetronikhd@gmail.com>
2023-12-21 13:17:57 +0100

    remove try catch

--- SoundBoard.py
+++ SoundBoard.py
@@ -3,12 +3,9 @@ from utils.AudioPlayer import play_mp3
 
 
 def main():
-    try:
-        while True:
-            # Starten des mp3 Players und des Listeners der Buttons
-            play_mp3(listener())
-    except KeyboardInterrupt:
-        print("\nBye bye!")
+    while True:
+        # Starten des mp3 Players und des Listeners der Buttons
+        play_mp3(listener())
 
 
 # Starten der main Methode
