6c8cc752064efd2b6d8f7d5e3e11983a39bae970
sklepp <shirin.klepp@milchundzucker.de>
2023-12-21 10:56:30 +0100

    small change for reader output

--- utils/AudioPlayer.py
+++ utils/AudioPlayer.py
@@ -3,7 +3,7 @@ import time
 
 
 def play_mp3(mp3):
-    print('Currently playing:' + mp3)
+    print('Currently playing: "' + mp3 + '" ♬')
     soundPath = './Sounds/'
     player = MPyg321Player()
     player.play_song(soundPath + mp3)
