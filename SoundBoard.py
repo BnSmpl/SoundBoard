384a0f1ea9ee83297a4a8448cb9646ae09a0e650
Tronik <thetronikhd@gmail.com>
2023-12-21 14:26:25 +0100

    add try catch for main

--- SoundBoard.py
+++ SoundBoard.py
@@ -5,9 +5,12 @@ from utils.AudioPlayer import play_mp3
 # Main Methode, welche in einer Dauerschleife läuft,
 # bis das Programm durch Keyboard-Interrupt gestoppt wird.
 def main():
-    while True:
-        # Starten des mp3 Players und des Listeners der Buttons
-        play_mp3(listener())
+    try:
+        while True:
+            # Starten des mp3 Players und des Listeners der Buttons
+            play_mp3(listener())
+    except KeyboardInterrupt:
+        print("\nBye bye!")
 
 
 # Starten der main Methode
--- utils/ButtonMatrix.py
+++ utils/ButtonMatrix.py
@@ -49,21 +49,18 @@ def readLine(line, characters):
 
 def listener():
     # Start der Überwachung der Buttons
-    try:
-        while True:
-            charactersL1: str = readLine(L1, ["ahhhh.mp3", "beback.mp3", "boom.mp3", "A"])
-            charactersL2: str = readLine(L2, ["bruh.mp3", "calming.mp3", "clap.mp3", "B"])
-            charactersL3: str = readLine(L3, ["gae.mp3", "giveuup.mp3", "lenz.mp3", "C"])
-            charactersL4: str = readLine(L4, ["*", "0", "#", "D"])
-            time.sleep(0.1)
-            # Wenn ein Button gedrückt würde, wird ein Wert gesetzt (Dateiname) und dieser Wert wird returned
-            if charactersL1:
-                return charactersL1
-            if charactersL2:
-                return charactersL2
-            if charactersL3:
-                return charactersL3
-            if charactersL4:
-                return charactersL4
-    except KeyboardInterrupt:
-        print("\nBye bye!")
+    while True:
+        charactersL1: str = readLine(L1, ["ahhhh.mp3", "beback.mp3", "boom.mp3", "A"])
+        charactersL2: str = readLine(L2, ["bruh.mp3", "calming.mp3", "clap.mp3", "B"])
+        charactersL3: str = readLine(L3, ["gae.mp3", "giveuup.mp3", "lenz.mp3", "C"])
+        charactersL4: str = readLine(L4, ["*", "0", "#", "D"])
+        time.sleep(0.1)
+        # Wenn ein Button gedrückt würde, wird ein Wert gesetzt (Dateiname) und dieser Wert wird returned
+        if charactersL1:
+            return charactersL1
+        if charactersL2:
+            return charactersL2
+        if charactersL3:
+            return charactersL3
+        if charactersL4:
+            return charactersL4
